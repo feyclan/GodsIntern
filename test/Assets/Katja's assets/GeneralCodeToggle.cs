3b2453e5098a1bc538994d677e020ab31d64ba66
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2018-12-18 12:32:17 +0100

    Add script

---
+++ test/Assets/Katja's assets/GeneralCodeToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralCodeToggle : MonoBehaviour {
+
+public GameObject SelectedInputField;
+public string[] CorrectCode;
+public GameObject[] RelevantInputField;
+	
+	// Update is called once per frame
+	void Update () {
+		string TypedCode = SelectedInputField.GetComponent<InputField>().text;
+		for (int i = 0; i < CorrectCode.Length; i++){
+			if(TypedCode == CorrectCode[i]){
+				ToggleInputField(RelevantInputField[i]);
+			}
+		}
+	}
+	
+	void ToggleInputField(GameObject input){
+		input.SetActive(true);
+	}
+}
