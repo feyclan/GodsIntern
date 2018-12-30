b224d6cded9bfd932ac118e2092fe27535bd6537
nours <nours11@msn.com>
2018-12-30 23:43:06 +0100

    change animal color

--- test/Assets/testjes/shaderteest.cs
+++ test/Assets/testjes/shaderteest.cs
@@ -29,11 +29,11 @@ public class shaderteest : MonoBehaviour
     {
         //Fetch the Renderer from the GameObject
         Renderer rend = SelectedObject.GetComponent<Renderer>();
-        Shader shad = Shader.Find("Unlit/Color");
+        Shader shader1 = Shader.Find("Standard");
         //Set the main Color of the Material to blue
         //rend.material.SetPass(2);
         //rend.material.color = chosencolor;
-        rend.material.shader = shad;
+        rend.material.shader = shader1;
         rend.material.SetColor("_Color", chosencolor);
 
     }
