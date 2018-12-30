3098360ed1787f97e0ea6c66d458e9da2e5f0261
nours <nours11@msn.com>
2018-12-30 20:05:09 +0100

    shader script

--- test/Assets/testjes/shaderteest.cs
+++ test/Assets/testjes/shaderteest.cs
@@ -32,7 +32,7 @@ public class shaderteest : MonoBehaviour
         //Set the main Color of the Material to blue
         //rend.material.SetPass(2);
         //rend.material.color = chosencolor;
-        rend.material.SetColor("_color", chosencolor);
+        rend.material.SetColor("_Color", chosencolor);
     }
 
 }
