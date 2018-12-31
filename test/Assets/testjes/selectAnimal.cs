b7888cc6e5b7b10302357a8101191d8701acb34d
nours <nours11@msn.com>
2018-12-31 16:44:30 +0100

    new script

---
+++ test/Assets/testjes/selectAnimal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class selectAnimal : MonoBehaviour {
+
+    public InputField SelectedInputField;
+    public GameObject ActivatedInput;
+    public string[] CorrectAnimal;
+    public GameObject[] RelevantAnimal;
+
+
+    // Use this for initialization
+    void Start() {
+
+        SelectedInputField.onEndEdit.AddListener(delegate
+        {
+            ReadCode();
+        });
+            
+
+
+    }
+	
+	// Update is called once per frame
+	void ReadCode () {
+
+        string inputCode = SelectedInputField.text;
+        for (int i = 0; i < CorrectAnimal.Length; i++){
+            if(inputCode == CorrectAnimal[i])
+            {
+                ActivatedInput.SetActive(true);
+
+            }
+
+        }
+		
+	}
+}
