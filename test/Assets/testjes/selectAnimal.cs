360ba030518975dc00f91dfd4a4c1019661e907a
nours <nours11@msn.com>
2019-01-01 03:10:23 +0100

    color script
    input animal first then choose color;
    problem in code: selecting animal after making changes on other animal, will change the color of both animals
    
    (maakt uiteindelijk niet zoveel uit omdat het veranderen van dingen wss anders gaat proberen, dit is meer als een test)

--- test/Assets/testjes/selectAnimal.cs
+++ test/Assets/testjes/selectAnimal.cs
@@ -6,18 +6,27 @@ using UnityEngine;
 
 public class selectAnimal : MonoBehaviour {
 
-    public InputField SelectedInputField;
-    public GameObject ActivatedInput;
-    public string[] CorrectAnimal;
-    public GameObject[] RelevantAnimal;
-
+    public InputField SelectedInputField; //fill which animal
+    public GameObject ActivatedInput; //set color input active
+    public InputField ActivatedInputSelect; //read color input
+    public string[] CorrectAnimal; //list of correct animals input
+    public GameObject[] RelevantAnimal; //list of animalobjects
+    public string[] colorCode;
+    public Color[] colors;
 
     // Use this for initialization
     void Start() {
 
         SelectedInputField.onEndEdit.AddListener(delegate
         {
-            ReadCode();
+            string inputCode = SelectedInputField.text;
+            for (int i = 0; i < CorrectAnimal.Length; i++)
+            {
+                if (inputCode == CorrectAnimal[i])
+                {
+                    ReadAnimal(RelevantAnimal[i]);
+                }
+            }
         });
             
 
@@ -25,17 +34,27 @@ public class selectAnimal : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void ReadCode () {
+	void ReadAnimal(GameObject a) {
 
-        string inputCode = SelectedInputField.text;
-        for (int i = 0; i < CorrectAnimal.Length; i++){
-            if(inputCode == CorrectAnimal[i])
+        ActivatedInput.SetActive(true);
+        ActivatedInputSelect.onEndEdit.AddListener(delegate
+        {
+            string colorInput = ActivatedInputSelect.text;
+            for (int i = 0; i < colorCode.Length; i++)
             {
-                ActivatedInput.SetActive(true);
-
+                if (colorInput == colorCode[i]){
+                    Renderer rend = a.GetComponent<Renderer>();
+                    ChangeColor(rend, colors[i]);
+                }
             }
-
-        }
-		
+        });
 	}
+
+    void ChangeColor(Renderer a, Color b)
+    {
+        Shader shader1 = Shader.Find("Standard");
+        a.material.shader = shader1;
+        a.material.SetColor("_Color", b);
+    }
+    
 }
