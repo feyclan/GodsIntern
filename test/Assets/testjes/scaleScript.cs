e8295a008b9123ddbdcf12e82e3a7e0f79139759
nours <nours11@msn.com>
2019-01-06 03:12:57 +0100

    scale

---
+++ test/Assets/testjes/scaleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scaleScript : MonoBehaviour {
+
+
+    public GameObject SelectedObject;
+    public InputField SelectedInput;
+
+	// Use this for initialization
+	void Start () {
+
+        SelectedInput.onEndEdit.AddListener(delegate {
+            float size = float.Parse(SelectedInput.text);
+
+            UpdateScale(size); });
+
+	}
+	
+	// Update is called once per frame
+	void UpdateScale (float a) {
+
+        SelectedObject.transform.localScale *= a;
+	}
+}
