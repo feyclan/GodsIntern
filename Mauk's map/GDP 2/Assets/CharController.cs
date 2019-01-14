d80bea63707551d3d821a48ad9a872cda8e14557
mvanbeek1 <maukvanbeek78@gmail.com>
2019-01-14 15:28:08 +0100

    map almost done

--- Mauk's map/GDP 2/Assets/CharController.cs
+++ Mauk's map/GDP 2/Assets/CharController.cs
@@ -23,8 +23,8 @@ public class CharController : MonoBehaviour {
 	}
     void Move() {
         Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey") * 6;
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey") * 6;
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey") * 10;
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey") * 10;
 
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
 
