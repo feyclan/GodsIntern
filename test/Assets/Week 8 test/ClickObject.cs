7ec1ca011bacf40997ae0ea99a02d3278b04b144
nours <nours11@msn.com>
2019-01-16 15:02:56 +0100

    i8uhsdnv

--- test/Assets/Week 8 test/ClickObject.cs
+++ test/Assets/Week 8 test/ClickObject.cs
@@ -7,31 +7,62 @@ using UnityEngine.UI;
 public class ClickObject : MonoBehaviour {
 
     public InputField commandInput;
-    public GameObject commandInput2; 
-    
+    public GameObject commandInput2;
+
+    //public string[] commands;
+
+    public string[] colors;
+    public Color[] colors2;
+
+
     // Use this for initialization
 	void Start () {
-		
-	}
+ 
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//if animal moving, input should stay close, MoveField()
 	}
 
     private void OnMouseDown()
     {
         //Debug.Log("success");
         commandInput2.SetActive(true);
-        Debug.Log(this.gameObject.name);
-        Testing();
+        MoveField();
+        Tester();
     }
 
-    void Testing()
+    void MoveField()
     {
-        float a = this.gameObject.transform.localPosition.x;
-        Debug.Log(a);
-        commandInput2.transform.localPosition += new Vector3(2, 2, 2);
-        //this.gameObject.GetComponent
+        Vector3 a = this.gameObject.transform.localPosition;
+        //Debug.Log(a);
+        commandInput2.transform.localPosition = new Vector3(a.x + 665 + ((a.x + 385)*23) , -31, 0);
+    }
+
+
+    void ChangeColor(Color i)
+    {
+        Renderer rend = this.gameObject.GetComponent<Renderer>();
+        Shader shader1 = Shader.Find("Standard");
+        rend.material.shader = shader1;
+        rend.material.SetColor("_Color", i);
+
+    }
+
+    void Tester()
+    {
+        commandInput.onEndEdit.AddListener(delegate
+        {
+            string inputCode = commandInput.text;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                if (inputCode == colors[i])
+                {
+                    ChangeColor(colors2[i]);
+                } 
+            }
+        });
     }
 }
