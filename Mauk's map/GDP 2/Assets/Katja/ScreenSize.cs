79b90482feac0ad57cb1f767f277e366121ea6b0
nours <nours11@msn.com>
2019-01-23 21:09:26 +0100

    HAHJKFAFD

---
+++ Mauk's map/GDP 2/Assets/Katja/ScreenSize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour {
+	
+public GameObject QuitScreen;
+	// Use this for initialization
+	void Start () {
+         Screen.SetResolution(1920,1080,true); // Change 3rd parameter to true for full screen
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+        QuitScreen.SetActive(true);
+    }
+	}
+}
